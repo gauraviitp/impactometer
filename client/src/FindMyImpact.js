05b2a128733b6dba5c0664ed1b19eac8af81bfe9
Gaurav Choudhary <cgauraviitp@gmail.com>
2020-10-01 17:26:18 +0100

    Added radio button component

--- client/src/FindMyImpact.js
+++ client/src/FindMyImpact.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import RadioButton from "./components/RadioButton";
 
-const FindMyImpact = () => {
-  return <h2>Find My Impact </h2>;
+const FindMyImpact = (props) => {
+  const [state, setState] = useState({ selection: false });
+
+  const onSelection = (event) => {
+    setState({ selection: event.target.value });
+  };
+
+  return (
+    <div className="container-fluid mt-5">
+      <h2>Find My Impact </h2>
+      <h4 className="mt-3">Question 1</h4>
+      <RadioButton
+        id="1"
+        onSelection={onSelection}
+        checked={state.selection === "1"}
+        labelText="option 1"
+        value="1"
+      />
+      <RadioButton
+        id="2"
+        onSelection={onSelection}
+        checked={state.selection === "2"}
+        labelText="option 2"
+        value="2"
+      />
+    </div>
+  );
 };
 
 export default FindMyImpact;
---
+++ client/src/components/RadioButton.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const RadioButton = (props) => {
+  return (
+    <div className="form-check">
+      <input
+        id={props.id}
+        type="radio"
+        className="form-check-input"
+        checked={props.checked}
+        onChange={props.onSelection}
+        value={props.value}
+      />
+      <label className="form-check-label" htmlFor={props.id}>
+        {props.labelText}
+      </label>
+    </div>
+  );
+};
+
+export default RadioButton;
