690462ce34b4e7f1cd246ab5a959d823e45595bb
Gaurav Choudhary <cgauraviitp@gmail.com>
2020-10-01 18:29:38 +0100

    Find my impact page changes for generic button creation

--- client/src/FindMyImpact.js
+++ client/src/FindMyImpact.js
@@ -2,30 +2,64 @@ import React, { useState } from "react";
 import RadioButton from "./components/RadioButton";
 
 const FindMyImpact = (props) => {
-  const [state, setState] = useState({ selection: false });
+  const initState = {
+    questions: [
+      {
+        id: "1",
+        text: "Question 1",
+        options: [
+          { id: "1", text: "Option 1" },
+          { id: "2", text: "Option 2" },
+        ],
+        selectedOption: "1",
+      },
+      {
+        id: "2",
+        text: "Question 2",
+        options: [
+          { id: "1", text: "Option 1" },
+          { id: "2", text: "Option 2" },
+        ],
+        selectedOption: "1",
+      },
+    ],
+  };
 
-  const onSelection = (event) => {
-    setState({ selection: event.target.value });
+  const [state, setState] = useState(initState);
+
+  const onSelection = (questionId, optionId) => {
+    console.log(`${questionId}, ${optionId}`);
+    const questions = [];
+    for (let question of state.questions) {
+      if (question.id === questionId) {
+        questions.push(
+          Object.assign({}, question, { selectedOption: optionId })
+        );
+      } else {
+        questions.push(question);
+      }
+    }
+    setState(Object.assign({}, state, { questions }));
   };
 
   return (
     <div className="container-fluid mt-5">
       <h2>Find My Impact </h2>
-      <h4 className="mt-3">Question 1</h4>
-      <RadioButton
-        id="1"
-        onSelection={onSelection}
-        checked={state.selection === "1"}
-        labelText="option 1"
-        value="1"
-      />
-      <RadioButton
-        id="2"
-        onSelection={onSelection}
-        checked={state.selection === "2"}
-        labelText="option 2"
-        value="2"
-      />
+      {state.questions.map((question) => (
+        <div key={question.id}>
+          <h4 className="mt-3">{question.text}</h4>
+          {question.options.map((option) => (
+            <RadioButton
+              id={`${question.id.toString()}-${option.id.toString()}`}
+              key={`${question.id.toString()}-${option.id.toString()}`}
+              onSelection={() => onSelection(question.id, option.id)}
+              checked={question.selectedOption === option.id}
+              labelText={option.text}
+              value={option.id}
+            />
+          ))}
+        </div>
+      ))}
     </div>
   );
 };
